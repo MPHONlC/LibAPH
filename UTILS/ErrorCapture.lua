286ab8bda076e866f90f995411093b96ee6a265b
APHONIC <207844175+MPHONlC@users.noreply.github.com>
2026-09-15 23:41:46 +0800

    Add time sync error codes to error capture

--- UTILS/ErrorCapture.lua
+++ UTILS/ErrorCapture.lua
@@ -4,10 +4,12 @@
 
 assert(LibAPH, "LibAPH.lua must be loaded before this file")
 
+local TIME_SYNC_ERROR_CODES = { [0x32BBA739] = true, [0xEA5D75AD] = true }
+
 function LibAPH.HookErrorCapture(addonName, onCaptured)
 	EVENT_MANAGER:RegisterForEvent(addonName .. "_LibAPH_ErrorCapture", EVENT_LUA_ERROR, function(_, errorString, errorCode)
 		if type(errorString) ~= "string" then return end
-		if errorCode == 0x32BBA739 then return end
+		if TIME_SYNC_ERROR_CODES[errorCode] then return end
 		if string.find(errorString, addonName, 1, true) then
 			onCaptured(errorString)
 		end
